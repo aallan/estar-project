aaeb3df7158707342dc75c78c9c0396f2333b84d
Alasdair Allan <alasdair@babilim.co.uk>
2005-02-07 23:03:12 +0000

    Removed debugging statements from ThreadOnAccept.pm, Perl 5.8.6 may have cured the hanging threads problems for us...

--- lib/perl5/SOAP/Transport/HTTP/Daemon/ThreadOnAccept.pm
+++ lib/perl5/SOAP/Transport/HTTP/Daemon/ThreadOnAccept.pm
@@ -15,14 +15,14 @@ sub handle {
   my $self = shift->new;
 
   while ( my $c = $self->accept) {
-    print "Main: Accepting connection...\n";
-    print "Main: Creating thread...\n";
+#    print "Main: Accepting connection...\n";
+#    print "Main: Creating thread...\n";
     my $handler_thread = threads->create( sub { callback( $self, $c ) } );
-    print "Main: detaching thread...\n";
+#    print "Main: detaching thread...\n";
     $handler_thread->detach();
-    print "Main: Going to sleep for 5 seconds...\n";
-    sleep(5);
-    print "Main: Waking up, \$c should go out of scope now...\n";
+#    print "Main: Going to sleep for 5 seconds...\n";
+#    sleep(5);
+#    print "Main: Waking up, \$c should go out of scope now...\n";
     #$c->close;
     #next;
   }
@@ -33,22 +33,22 @@ sub callback {
   my $self = shift;
   my $c = shift;
   
-  print "Thread: In callback() function...\n";
-  print "Thread: Closing listening socket...\n";
+#  print "Thread: In callback() function...\n";
+#  print "Thread: Closing listening socket...\n";
   $self->close;  # Close the listening socket (always done in children)
-  print "Thread: Socket closed...\n";
+#  print "Thread: Socket closed...\n";
 
   # Handle requests as they come in
   while (my $r = $c->get_request) {
-    print "Thread: Handling request...\n";
+#    print "Thread: Handling request...\n";
     $self->request($r);
     $self->SOAP::Transport::HTTP::Server::handle;
-    print "Thread: Sending response...\n";
+#    print "Thread: Sending response...\n";
     $c->send_response($self->response);
   }
-  print "Thread: Closing socket...\n";
+#  print "Thread: Closing socket...\n";
   $c->close;
-  print "Thread: Socket closed, returning...\n";
+#  print "Thread: Socket closed, returning...\n";
   return;
 }
 
