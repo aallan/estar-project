12618073989ce88ba235514f67f9f8c970b69f07
Alasdair Allan <alasdair@babilim.co.uk>
2005-05-10 00:25:10 +0000

    Updated ogle_fetch.pl to retrieve and parse KDH's web pages

--- bin/ogle_fetch.pl
+++ bin/ogle_fetch.pl
@@ -20,7 +20,7 @@ Alasdair Allan (aa@astro.ex.ac.uk)
 
 =head1 REVISION
 
-$Id: ogle_fetch.pl,v 1.1 2005/05/09 13:44:19 aa Exp $
+$Id: ogle_fetch.pl,v 1.2 2005/05/10 00:25:10 aa Exp $
 
 =head1 COPYRIGHT
 
@@ -37,7 +37,7 @@ use vars qw / $VERSION /;
 #  Version number - do this before anything else so that we dont have to 
 #  wait for all the modules to load - very quick
 BEGIN {
-  $VERSION = sprintf "%d.%d", q$Revision: 1.1 $ =~ /(\d+)\.(\d+)/;
+  $VERSION = sprintf "%d.%d", q$Revision: 1.2 $ =~ /(\d+)\.(\d+)/;
  
   #  Check for version number request - do this before real options handling
   foreach (@ARGV) {
@@ -70,6 +70,7 @@ use eSTAR::UserAgent;
 # general modules
 #use SOAP::Lite +trace => all;  
 use SOAP::Lite;
+use POSIX qw(:sys_wait_h);
 use Errno qw(EWOULDBLOCK EINPROGRESS);
 use Net::Domain qw(hostname hostdomain);
 use Fcntl qw(:DEFAULT :flock);
@@ -82,7 +83,8 @@ use Sys::Hostname;
 use Config::User;
 use Getopt::Long;
 use Data::Dumper;
-
+use Config;
+use Socket;
 
 # tag name of the current process, this identifies where log and 
 # status files for this process will be stored.
@@ -217,7 +219,8 @@ if ( $config->get_state("of.unique_process") == 1 ) {
    $config->set_option("of.passwd", "InterProcessCommunication" );
    
    # ogle page
-   $config->set_option("of.url", "cgi.st-andrews.ac.uk/cgi/~cdbs/optimise.pl");
+   $config->set_option("of.remote", "cgi.st-andrews.ac.uk");
+   $config->set_option("of.url", "cgi/~cdbs/optimise.pl");
 
    # connection options defaults
    $config->set_option("connection.timeout", 5 );
@@ -228,6 +231,30 @@ if ( $config->get_state("of.unique_process") == 1 ) {
    $config->set_option("mailhost.domain", 'astro.ex.ac.uk' );
    $config->set_option("mailhost.timeout", 30 );
    $config->set_option("mailhost.debug", 0 );   
+   
+   # science (defaults are for LT)
+   $config->set_option("science.long", "-17.88166" );
+   $config->set_option("science.lat", "28.76" );
+   $config->set_option("science.elev", "2326" );
+   $config->set_option("science.Vmean", "20.0" );
+   $config->set_option("science.k", "0.1" );
+   $config->set_option("science.aperture", "2.0" );
+   $config->set_option("science.thruput", "60" );
+   $config->set_option("science.bandwidth", "1410" );
+   $config->set_option("science.QE", "90" );
+   $config->set_option("science.pixel", "0.27" );
+   $config->set_option("science.gain", "2.6" );
+   $config->set_option("science.ron", "2.5" );
+   $config->set_option("science.bias", "1200" );
+   $config->set_option("science.sat", "60000" );
+   $config->set_option("science.tread", "10" );
+   $config->set_option("science.slew", "30" );
+   $config->set_option("science.data", "PLENS" );
+   $config->set_option("science.time", "TONIGHT" );
+   $config->set_option("science.seeing", "1.0" );
+   $config->set_option("science.q", "0.001" );
+   $config->set_option("science.dchi", "25" );
+   $config->set_option("science.hours", "2.0" );
     
    # C O M M I T T   O P T I O N S  T O   F I L E S
    # ----------------------------------------------
@@ -251,7 +278,7 @@ my $lwp = new LWP::UserAgent(
 # Configure User Agent                         
 $lwp->env_proxy();
 $lwp->agent( "eSTAR OGLE Fetch /$VERSION (" 
-             . hostname() . "." . hostdomain() .")" );
+             . hostname() . ")" );
 
 my $ua = new eSTAR::UserAgent(  );  
 $ua->set_ua( $lwp );
@@ -271,6 +298,8 @@ my $status = GetOptions( "host=s"     => \$opt{"host"},
                          "elev=s"     => \$opt{"elev"},
                          "hours=s"    => \$opt{"hours"},
                           );
+
+# N O N - S C I E N C E   D E F A U L T S -----------------------------------
 
 # default hostname
 unless ( defined $opt{"host"} ) {
@@ -300,22 +329,221 @@ unless( defined $opt{"port"} ) {
 unless( defined $opt{"user"} ) {
    $opt{"user"} = $config->get_option("of.user");
 } else{       
-   $log->warn("Warning: Resetting username from " .
-             $config->get_option("of.user") . " to $opt{user}");
-   $config->set_option("of.user", $opt{"user"} );
+   if ( defined $config->get_option("of.user") ) {
+      $log->warn("Warning: Resetting username from " .
+                $config->get_option("of.user") . " to $opt{user}");
+      $config->set_option("of.user", $opt{"user"} );
+   }
 }
 
 # default user and password location
 unless( defined $opt{"pass"} ) {
    $opt{"pass"} = $config->get_option("of.passwd");
 } else{       
-   $log->warn("Warning: Resetting password...");
-   $config->set_option("of.passwd", $opt{"pass"} );
+   if ( defined $config->get_option("of.passwd") ) {
+      $log->warn("Warning: Resetting password...");
+      $config->set_option("of.passwd", $opt{"pass"} );
+   }
 }
+
+
+# S C I E N C E   D E F A U L T S --------------------------------------------
+
+unless( defined $opt{"long"} ) {
+   $opt{"long"} = $config->get_option("science.long");
+   $log->warn( "Using default longitude of $opt{long}" );
+} else{       
+   if ( defined $config->get_option("science.long") ) {
+      $log->debug("Resetting longitude from " .
+                $config->get_option("science.long") . " to $opt{long}");
+      $config->set_option("science.long", $opt{"long"} );
+   }   
+}
+
+unless( defined $opt{"lat"} ) {
+   $opt{"lat"} = $config->get_option("science.lat");
+   $log->warn( "Using default latitude of $opt{lat}" );
+} else{       
+   if ( defined $config->get_option("science.lat") ) {
+      $log->debug("Resetting latitude from " .
+                $config->get_option("science.lat") . " to $opt{lat}");
+      $config->set_option("science.lat", $opt{"lat"} );
+   }   
+}
+
+unless( defined $opt{"elev"} ) {
+   $opt{"elev"} = $config->get_option("science.elev");
+   $log->warn( "Using default elevation of $opt{elev}" );
+} else{       
+   if ( defined $config->get_option("science.elev") ) {
+      $log->debug("Resetting elevation from " .
+                $config->get_option("science.elev") . " to $opt{elev}");
+      $config->set_option("science.elev", $opt{"elev"} );
+   }   
+}
+
+unless( defined $opt{"hours"} ) {
+   $opt{"hours"} = $config->get_option("science.hours");
+   $log->warn( "Using default value of $opt{hours}hr for number of hours" );
+} else{       
+   if ( defined $config->get_option("science.hours") ) {
+      $log->debug("Resetting hours per night from " .
+                $config->get_option("science.hours") . " to $opt{hours}");
+      $config->set_option("science.hours", $opt{"hours"} );
+   }   
+}
+
+# B U I L D   U R L ----------------------------------------------------------
+
+$log->debug("Building request..." );
+
+my $query = [ "long"        => $config->get_option("science.long"),
+              "lat"         => $config->get_option("science.lat"),
+              "elev"        => $config->get_option("science.elev"),
+              "Vmean"       => $config->get_option("science.Vmean"),
+              "k"           => $config->get_option("science.k"),
+              "aperture"    => $config->get_option("science.aperture"),
+              "thruput"     => $config->get_option("science.thruput"),
+              "bandwidth"   => $config->get_option("science.bandwidth"),
+              "QE"          => $config->get_option("science.QE"),
+              "pixel"       => $config->get_option("science.pixel"),
+              "gain"        => $config->get_option("science.gain"),
+              "ron"         => $config->get_option("science.ron"),
+              "bias"        => $config->get_option("science.bias"),
+              "sat"         => $config->get_option("science.sat"),
+              "tread"       => $config->get_option("science.tread"),
+              "slew"        => $config->get_option("science.slew"),
+              "data"        => $config->get_option("science.data"),
+              "time"        => $config->get_option("science.time"),
+              "seeing"      => $config->get_option("science.seeing"),
+              "q"           => $config->get_option("science.q"),
+              "dchi"        => $config->get_option("science.dchi"),
+              "hrspernight" => $config->get_option("science.hours") ];
+
+my $url = "http://" . $config->get_option("of.remote") . "/" . 
+          $config->get_option("of.url");
 
 # F E T C H   O G L E   U R L ------------------------------------------------
 
-my $request = new HTTP::Request('GET', $url);
-my $reply = $ua->get_ua()->request($request);
+$log->debug("URL = $url" );
+$log->debug("Fetching page..." );
+my $reply;
+eval { $reply = $ua->get_ua()->post( $url, $query ) };
+
+# we're fucked, live with it
+if ( $@ ) {
+   $log->error( "Error: $@" );
+   $log->error( "Exiting with bad status..." );
+   throw eSTAR::Error::FatalError($error, ESTAR__FATAL);
+}   
+
+# We successfully made a request but it returned bad status
+unless ( ${$reply}{"_rc"} eq 200 ) {
+  # the network connection failed?      
+  $log->error( "Error: (${$reply}{_rc}): ${$reply}{_msg}");
+  throw eSTAR::Error::FatalError($error, ESTAR__FATAL);
+}
+
+# we should have a reply
+my @page = split "\n", ${$reply}{_content};
+
+print Dumper( @page );
+
+# Validate our reply, certain strings should be in certain places. If 
+# they aren't in the right place then 
+
+# use the headers from line 11 of the page
+my @headers = split "<td>", $page[10];
+
+# get rid of the first <tr>
+shift @headers;
+
+# get rid of the trailing <tr>
+$headers[10] =~ s/<\/tr>//;
+
+foreach my $i ( 0 ... $#headers ) {
+   $headers[$i] =~ s/^\s+//g;
+   $headers[$i] =~ s/\s+$//g;
+}
+print Dumper( @headers );
+
+# check things, if they aren't right throw an error
+unless ( $headers[0] eq "rank" &&
+         $headers[1] eq "event" &&
+         $headers[2] eq "mag" &&
+         $headers[3] eq "A" &&
+         $headers[4] eq "t-t0" &&
+         $headers[5] eq "t_E" &&
+         $headers[6] eq "nx(read+exp)= tobs" &&
+         $headers[7] eq "S/N" &&
+         $headers[8] eq "g" &&
+         $headers[9] eq "W" &&
+         $headers[10] eq "Chi/N"  ) {
+   my $error = "Error: Page does not parse correctly";
+   $log->error( "Retrieved HTML:\n" );
+   foreach my $i ( 0 ... $#page ) {
+      $log->error( $page[$i] );
+   }         
+   $log->error( $error );
+   throw eSTAR::Error::FatalError($error, ESTAR__FATAL);
+}   
+
+# loop through the remaining part of the table and discard any rows which
+# aren't data (the repeated "rank" lines are annoying special cases and 
+# have to be removed from the list).
+my @data;
+foreach my $j ( 11 ... $#page ) {
+   last if $page[$j] =~ "</table>"; # end of the data table
+   next if $page[$j] =~ "rank";     # non-data line
+   
+   my @columns = split "<td>", $page[$j];
+   shift @columns;
+   $columns[10] =~ s/<\/tr>//;
+   
+   foreach my $k ( 0 ... $#columns ) {
+      $columns[$k] =~ s/^\s+//g;
+      $columns[$k] =~ s/\s+$//g;
+   }  
+    
+
+
+   # grab the unique event name which we'll use as a key
+   my $key = $columns[1];
+   $key =~ 
+    s/<a href=http:\/\/star-www.st-and.ac.uk\/~kdh1\/cool\/blg-\d+.html>//;
+   $key =~ s/<\/a>//;
+   
+   # get URL
+   my $url = $columns[1];
+   $url =~ s/<a href=//;
+   $url =~ s/>OGLE-\n+-blg-\n+<\/a>//;
+   
+   # get number of exposures
+   my $num = $columns[6];
+   $num =~ s/x\(\d+\+\d+\)=\d+s$//;
+   
+   # get exposure time
+   my $read = $columns[6];
+   $read =~ s/^\d+x\(//;
+   $read =~ s/\+\d+\)=\d+s$//;
+   my $exp = $columns[6];
+   $exp =~ s/^\d+x\(\d+\+//;
+   $exp =~ s/\)=\ds$//; 
+   
+   my $time = $read + $exp; 
+   
+   # build the hash entry
+   my $ref = { ID => $key, URL => $url, Number => $num, Time => $time };
+   push @data, $ref;
+   
+   #print "\$j = $j\n\n";
+   #print Dumper( @columns );
+   #print "\nKey = $key\nURL = $url\nNum = $num\nRead = $read\nExp = $exp\nTime = $time\n";
+   #print "\n\n\n";
+
+}
+
+print Dumper ( @data );
+
   
 exit;
