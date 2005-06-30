07397e1431a12938fb27b70133904c021a33ee03
Alasdair Allan <alasdair@babilim.co.uk>
2005-06-30 20:54:00 +0000

    shipping to mountain

--- bin/correlation_daemon.pl
+++ bin/correlation_daemon.pl
@@ -15,7 +15,7 @@ my $status;
 #  Version number - do this before anything else so that we dont have to 
 #  wait for all the modules to load - very quick
 BEGIN {
-  $VERSION = sprintf "%d.%d", q$Revision: 1.59 $ =~ /(\d+)\.(\d+)/;
+  $VERSION = sprintf "%d.%d", q$Revision: 1.60 $ =~ /(\d+)\.(\d+)/;
  
   #  Check for version number request - do this before real options handling
   foreach (@ARGV) {
@@ -436,7 +436,7 @@ sub correlate {
   # in a case where we only have one filter so this isn't a problem.
   $log->debug( "Grabbing filter from first catalogue item...");
   my $star = $catalogs[0]->starbyindex(0);
-  my @waveband = $star->whatfilters();
+  my @waveband = $star->what_filters();
   if ( defined $waveband[0] ) {
      $log->debug( "Setting \$OPT{filter} to be '".$waveband[0]."'");
      $OPT{filter} = $waveband[0];
