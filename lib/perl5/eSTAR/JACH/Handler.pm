5b7a457caa63869d1c631bd9f7506f3ef1b31998
Alasdair Allan <alasdair@babilim.co.uk>
2007-01-03 14:12:29 +0000

    bug fix shipping to mountain

--- lib/perl5/eSTAR/JACH/Handler.pm
+++ lib/perl5/eSTAR/JACH/Handler.pm
@@ -717,7 +717,7 @@ sub handle_rtml {
         
          # Offset the target MSB by -795 arcsec in both RA and Dec (S & W)
          my $instrument = eSTAR::JACH::Util::current_instrument( 
-                                    $config->get_option( "dn.telescope"); 
+                                    $config->get_option( "dn.telescope")); 
          $log->debug( "The current instrument is $instrument" );
          if ( $instrument == "WFCAM" ) {                           
            $log->warn( "Warning: The current instrument is $instrument" );
