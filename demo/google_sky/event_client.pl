fd850d4012564b845e69bf0cf8a9088faf02a7e2
Alasdair Allan <alasdair@babilim.co.uk>
2007-08-22 16:35:09 +0000

    updated

--- demo/google_sky/event_client.pl
+++ demo/google_sky/event_client.pl
@@ -11,7 +11,7 @@ use threads::shared;
 #  Version number - do this before anything else so that we dont have to 
 #  wait for all the modules to load - very quick
 BEGIN {
-  $VERSION = sprintf "%d.%d", q$Revision: 1.1 $ =~ /(\d+)\.(\d+)/;
+  $VERSION = sprintf "%d.%d", q$Revision: 1.2 $ =~ /(\d+)\.(\d+)/;
  
   #  Check for version number request - do this before real options handling
   foreach (@ARGV) {
@@ -55,16 +55,10 @@ use Config;
 use Socket;
 use Data::Dumper;
 use Carp;
-
-use Starlink::AST;
-use Starlink::AST::Tk;
-
-use Astro::FITS::CFITSIO;
-use Astro::FITS::Header::CFITSIO;
+use Net::FTP;
 
 use Astro::VO::VOEvent;
-use XMLRPC::Lite;
-use XMLRPC::Transport::HTTP;
+
 
 # tag name of the current process, this identifies where log and 
 # status files for this process will be stored.
@@ -334,8 +328,18 @@ sub event_process {
 	      #print "$response\n";
               $sock->flush(); 
 	      
-	      if ( $object->role() eq "observation" ||
-	           $object->role() eq "test" ) {
+	      if ( $object->role() eq "observation" || $object->role() eq "test" ) {
+
+                my $id;
+                eval { $id = $object->id( ); };
+                if ( $@ ) {
+   	           my $error = "$@";
+   	           chomp( $error );
+   	           $log->error( "Error: $error" );
+   	           $log->error( "\$data = " . $message );
+   	           next;
+                } 
+                $log->debug( "ID: $id" );
 		   
 		my $ra = $object->ra();
 		my $dec = $object->dec();
@@ -344,8 +348,87 @@ sub event_process {
   		   $log->print( "RA = $ra, Dec = $dec" );
                    my $long = $ra - 180;
                    $log->debug( "RA - 180 deg = $long" );
-             
-		
+
+                   # Writing to voevent.kml file
+                   my $state_dir = File::Spec->catdir( $config->get_state_dir() );  
+                   my $kml = File::Spec->catfile( $state_dir, "voevent.kml" );
+
+                   # write the observation object to disk.
+                   # -------------------------------------
+   
+                   if ( open ( KML, "$kml" )) {
+                       
+		      $log->warn( "Slurping from $kml" );
+		      my @lines;
+		      {
+		         $/ = "\n";
+		         @lines = <KML>;
+	              }		 
+		      close( KML );
+		      
+		      $log->debug("Updating content" );
+		      my $string = 
+		      '  <Placemark>'."\n".
+		      '    <name>'.$id.'</name>'."\n".
+		      '    <description>'.$object->description().'</description>'."\n".
+		      '    <Point>'."\n".
+		      '      <coordinates>'.$long.','.$dec.',0</coordinates>'."\n".
+		      '    </Point>'."\n".
+		      '  </Placemark>'."\n".
+		      '  </Folder>'."\n";
+		      
+		      my $line = $#lines - 1;
+		      $lines[$line] = $string;
+		      
+		      $log->warn( "Unlinking old file...");
+		      unlink $kml;
+		      
+		      $log->warn( "Writing new KML file...");
+		      open ( KML, ">$kml" );
+		      foreach my $i ( 0 ... $#lines ) {
+		         print KML $lines[$i];
+		      }
+		      close( KML );	  
+		       
+                   } else {
+		      $log->warn( "Creating $kml" );
+		      open ( KML, ">$kml" );
+		      	                 
+		      my $string = '<?xml version="1.0" encoding="UTF-8"?>'."\n".
+		      '<kml xmlns="http://earth.google.com/kml/2.1">'."\n".
+		      '  <Folder>'."\n".
+		      '  <Placemark>'."\n".
+		      '    <name>'.$id.'</name>'."\n".
+		      '    <description>'.$object->description().'</description>'."\n".
+		      '    <Point>'."\n".
+		      '      <coordinates>'.$long.','.$dec.',0</coordinates>'."\n".
+		      '    </Point>'."\n".
+		      '  </Placemark>'."\n".
+		      '  </Folder>'."\n".
+		      '</kml>'."\n";
+                      $log->debug("Writing new KML file");
+		      print KML $string;
+		      close ( KML );
+                   }
+		   
+		   eval { 
+                      $log->print("Opening FTP connection to lion.drogon.net...");  
+                      my $ftp = Net::FTP->new( "lion.drogon.net", Debug => 1 );
+                      $log->debug("Going into PASV mode...");
+                      $log->debug("Logging into estar account...");  
+                      $ftp->login( "estar", "tibileot" );
+                      $ftp->cwd( "www.estar.org.uk/docs/voevent/" );
+                      $log->debug("Transfering file $kml");  
+                      $ftp->put( $kml, "voevent.kml" );
+                      $ftp->quit();
+	              $log->print("Closed FTP connection...");
+                   };
+
+                   if ( $@ ) {
+                     my $error = "$@";
+                     chomp $error;
+                     $log->error( "Error: $error" );
+                   }	   
 
 	        } else {
 		   $log->warn( "Warning: Unable to understand the RA&Dec" );
